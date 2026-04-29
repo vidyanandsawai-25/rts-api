bdf8b145bf2700d302d9dade9ce7cb3ee66d0994
PrajvalBhoyar <it.prajvalb@sthapatya.in>
2026-04-29 16:25:35 +0530

    Rename BankId property to Id in BankQueryParameters class (#301)

--- src/Application/NtisPlatform.Application/DTOs/Master/BankMaster/BankQueryParameters.cs
+++ src/Application/NtisPlatform.Application/DTOs/Master/BankMaster/BankQueryParameters.cs
@@ -8,7 +8,7 @@ namespace NtisPlatform.Application.DTOs.Master.BankMaster
     {
         [Filterable]
         [Sortable]
-        public int? BankId { get; set; }
+        public int? Id { get; set; }
 
         [Filterable(FilterOperator.Contains)]
         [Searchable]
