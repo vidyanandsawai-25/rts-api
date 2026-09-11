760a29eb543698ec185084a36a412e9036a3bea3
Vidyanand Sawai <it.vidyanands@sthapatya.in>
2026-09-11 12:43:41 +0530

    feat(rts): add complete CRUD endpoints (GetById, Create, Update, Delete) to RTSDepartmentController

--- src/Presentation/NtisPlatform.Api/Controllers/Master/RTSDepartmentController.cs
+++ src/Presentation/NtisPlatform.Api/Controllers/Master/RTSDepartmentController.cs
@@ -25,4 +25,19 @@ public class RTSDepartmentController : ControllerBase
     public Task<IActionResult> GetAll([FromQuery] RTSDepartmentQueryParameters queryParameters, CancellationToken ct)
         => this.ExecuteGetAllPaged(_service, queryParameters, _logger, ct);
 
+    [HttpGet("{id}")]
+    public Task<IActionResult> GetById(int id, CancellationToken ct)
+        => this.ExecuteGetById(_service, id, _logger, ct);
+
+    [HttpPost]
+    public Task<IActionResult> Create([FromBody] CreateRTSDepartmentDto createDto, CancellationToken ct)
+        => this.ExecuteCreate(_service, createDto, _logger, ct);
+
+    [HttpPut("{id}")]
+    public Task<IActionResult> Update(int id, [FromBody] UpdateRTSDepartmentDto updateDto, CancellationToken ct)
+        => this.ExecuteUpdate(_service, id, updateDto, _logger, ct);
+
+    [HttpDelete("{id}")]
+    public Task<IActionResult> Delete(int id, CancellationToken ct)
+        => this.ExecuteDelete(_service, id, _logger, ct);
 }
